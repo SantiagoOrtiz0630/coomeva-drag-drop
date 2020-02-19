18f52b24abafc15f3a675beebbc484add8e4f51e
Sebastian Castaño Jara <sebastian.caja@gmail.com>
2020-02-19 09:48:45 -0500

    fixed interaction

--- js/main.js
+++ js/main.js
@@ -1,20 +1,37 @@
-let listContainer = document.getElementsByClassName("App-list");
+let listContainer = document.getElementsByClassName("App-List");
 
-var mySortableContainer = Sortable.create(listContainer[0], {
+let mainListContainer = document.getElementsByClassName("App-MainList")[0];
+
+let sortContainer = [];
+let mainSortContainer = null;
+
+mainSortContainer = Sortable.create(mainListContainer, {
+    animation: 150,
+    ghostClass: 'gray-item-class',
+    group: 'main',
+});
+
+sortContainer[0] = Sortable.create(listContainer[0], {
     animation: 150,
     ghostClass: 'blue-item-class',
     group: 'main',
 });
 
-var mySortableContainer = Sortable.create(listContainer[1], {
+sortContainer[1] = Sortable.create(listContainer[1], {
     animation: 150,
     ghostClass: 'green-item-class',
     group: 'main',
 });
 
-var mySortableContainer = Sortable.create(listContainer[2], {
+sortContainer[2] = Sortable.create(listContainer[2], {
     animation: 150,
     ghostClass: 'red-item-class',
+    group: 'main',
+});
+
+sortContainer[3] = Sortable.create(listContainer[3], {
+    animation: 150,
+    ghostClass: 'yellow-item-class',
     group: 'main',
 });
 
